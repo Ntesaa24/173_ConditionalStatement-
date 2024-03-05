68edc150935bac40ad768550c6d278b917333c3f
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-03-05 13:36:00 +0700

    pertemuan2.3

--- 123/123.cpp
+++ 123/123.cpp
@@ -1,9 +1,30 @@
-// 123.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
-using namespace 
+using namespace std; 
 int main()
 
+{
+	int nilaiMatematika, nilaiFisika, nRerata; 
+	string status; 
+	cout << "Masukan Nilai Matematika : ";
+	cin >> nilaiMatematika;
+	cout << "Masukan Nilai Fisika: ";
+	cin >> nilaiFisika; 
+	nRerata = (nilaiMatematika + nilaiFisika) / 2; 
+	
+	if (nRerata > 60) {
+		status = "Lulus";
+}
+	else if (nilaiMatematika > 70) {
+		status = "Lulus";
+}
 
+	else {
+		status = "Tidak Lulus";
+}
+
+cout << "Nilai MTK : " << nilaiMatematika << endl; 
+cout << "Nilai Fisika : " << nilaiFisika << endl; 
+cout << "Rata-rata nilai " << status << endl; 
+return 0; 
 
